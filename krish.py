72be20e48b48fb51ee22479966938d698893628e
Krishnakumar M <krishna00i777@gmail.com>
2024-10-10 16:53:39 +0530

    Add files via upload

---
+++ krish.py
@@ -0,0 +1,15 @@
+my_name="krish"
+name = input("Enter your name ")
+
+def namechecker():
+    if name==my_name:
+        return True
+    else:
+        return False
+    
+print(namechecker())
+
+def add(a,b):
+    return a+b
+print(add(2,4))
+
